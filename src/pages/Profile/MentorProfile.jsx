7b90da3f49edb5edf094fca0d2a1b633becdd901
raZorpaul <nandriejoshua@gmail.com>
2025-03-05 18:39:43 +0300

    add mentor profile page

---
+++ src/pages/Profile/MentorProfile.jsx
@@ -0,0 +1,77 @@
+// MentorProfile.jsx
+import { useState } from "react";
+import { StructuredListBody, StructuredListWrapper } from "@carbon/react";
+import BasicInfoSection from "../sections/BasicInfoSection";
+import LocationSection from "../sections/LocationSection.jsx";
+import EmergencyContactSection from "../sections/EmergencyContactSection";
+import EducationSection from "../sections/EducationSection";
+import CertificationsSection from "../sections/CertificationsSection";
+import HobbiesSection from "../sections/HobbiesSection";
+import "./MentorProfile.scss";
+import HeaderComponent from "../Header with Navigation/HeaderComponent.jsx";
+import ProfileSidebar from "../ProfileSideBar/ProfileSidebar.jsx";
+
+const MentorProfile = ({ initialData }) => {
+  const [mentorData, setMentorData] = useState(initialData);
+
+  const handleUpdateSection = (section, newData) => {
+    setMentorData((prev) => ({
+      ...prev,
+      [section]: newData,
+    }));
+  };
+
+  return (
+    <div className="mentor-profile-container">
+      <HeaderComponent />
+      <div className="mentor-profile-content">
+        {/*<aside className="sidebar">*/}
+        {/*  <ProfileSidebar />*/}
+        {/*</aside>*/}
+        <main className="main-content">
+          {/*<h2 className="profile-section-title">Basic Information</h2>*/}
+          <StructuredListWrapper>
+            <StructuredListBody>
+              <BasicInfoSection
+                data={mentorData.basicInfo}
+                onUpdate={(newData) => handleUpdateSection("basicInfo", newData)}
+              />
+
+              <LocationSection
+                data={mentorData.locationOfResidence}
+                title="Location Of Residence"
+                onUpdate={(newData) => handleUpdateSection("location", newData)}
+                />
+
+              <LocationSection
+                data={mentorData.locationOfWork}
+                title="Location Of Work"
+                onUpdate={(newData) => handleUpdateSection("location", newData)}
+                />
+
+              <CertificationsSection
+                data={mentorData.certifications}
+                title="Certifications"
+                onUpdate={(newData) => handleUpdateSection("certifications", newData)}
+                />
+
+              <HobbiesSection
+                data={mentorData.hobbies}
+                title="Hobbies"
+                onUpdate={(newData) => handleUpdateSection("hobbies", newData)}
+                />
+
+              <EmergencyContactSection
+                data={mentorData.emergencyContact}
+                title="Emergency Contact"
+                onUpdate={(newData) => handleUpdateSection("emergencyContact", newData)}
+                />
+            </StructuredListBody>
+          </StructuredListWrapper>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default MentorProfile;
